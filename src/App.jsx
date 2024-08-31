74b862c5c48faa47fc6401ed9da762db16059b0d
dag14 <dagimasrat3@gmail.com>
2024-08-31 23:59:31 +0300

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import photo from "./assets/photo.jpg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className='flex flex-col w-1/4 border-red bg-green'>
-      <div className='h-1/2'>
-        <img src={photo} alt='' />
+    <>
+      <Router>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='forget' element={<Forget />} />
+          <Route
+            path='/password-reset-confirm/:uid/:token'
+            element={<Reset />}
+          />
+        </Routes>
+      </Router>
+      <div className='flex h-full flex-col w-1/4 border-red bg-green'>
+        <div className='h-1/2'>
+          <img src={photo} alt='' />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
